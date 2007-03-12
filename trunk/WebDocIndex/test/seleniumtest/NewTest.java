d3800bef757a9ee37de9c58aeed00735ddea372c
joliva1983 <joliva1983@c3f7b990-ec2a-0410-86c9-3fd7826a91b9>
2007-03-12 00:27:01 +0000

    Modificacion del test

--- trunk/WebDocIndex/test/seleniumtest/NewTest.java
+++ trunk/WebDocIndex/test/seleniumtest/NewTest.java
@@ -1,18 +1,35 @@
+
+
 package seleniumtest;
 
+import junit.framework.*;
+import org.openqa.selenium.server.*;
 import com.thoughtworks.selenium.*;
-import java.util.regex.Pattern;
 
+public class NewTest extends TestCase {
+    
+    private Selenium selenium;
 
-public class NewTest extends SeleneseTestCase {
-	public void testNew() throws Exception {
-		selenium.open("/WebDocIndex/signup.html");
-		selenium.type("email1", "pepeti@hoymail.com");
-		selenium.type("email2", "pepeti@hoymail.com");
-		selenium.type("password1", "12345");
-		selenium.type("password2", "12345");
-		selenium.click("submit");
-		selenium.waitForPageToLoad("30000");
-		checkForVerificationErrors();
-	}
+    public void setUp() throws Exception {
+        String url = "http://localhost:8080";
+        selenium = new DefaultSelenium("localhost", SeleniumServer.getDefaultPort(), "*firefox", url);
+        selenium.start();
+    }
+
+    protected void tearDown() throws Exception {
+        selenium.stop();
+    }
+
+    public void NewTest() throws Throwable {
+        selenium.open("/WebDocIndex/signup.html");
+        selenium.type("email1", "qwert@ha.com");
+        selenium.type("email2", "qwert@ha.com");
+        selenium.type("password1", "12345");
+        selenium.type("password2", "12345");
+        selenium.click("submit");
+        selenium.waitForPageToLoad("30000");
+        selenium.isTextPresent("Usuario Registrado con Exito"); //significa que el test ha pasado con exito
+        
+    }
+
 }
