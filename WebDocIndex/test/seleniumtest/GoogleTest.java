0a64dea3ef48e4a553cc73f5ce4b46af9834496f
joliva1983 <joliva1983@c3f7b990-ec2a-0410-86c9-3fd7826a91b9>
2007-03-12 00:26:39 +0000

    Test de prueba

---
+++ WebDocIndex/test/seleniumtest/GoogleTest.java
@@ -0,0 +1,34 @@
+package seleniumtest;
+
+import com.thoughtworks.selenium.*;
+
+import junit.framework.*;
+
+import org.openqa.selenium.server.*;
+
+public class GoogleTest extends TestCase {
+    private Selenium selenium;
+
+    public void setUp() throws Exception {
+        String url = "http://localhost:8080";
+        selenium = new DefaultSelenium("localhost", 4444, "*firefox", url);
+        selenium.start();
+        
+    }
+
+    protected void tearDown() throws Exception {
+        selenium.stop();
+    }
+
+    public void testGoogleTestSearch() throws Throwable {
+        selenium.open("/WebDocIndex/signup.html");
+        selenium.type("email1", "qwert@ha.com");
+        selenium.type("email2", "qwert@ha.com");
+        selenium.type("password1", "12345");
+        selenium.type("password2", "12345");
+        selenium.click("submit");
+        selenium.waitForPageToLoad("30000");
+        selenium.isTextPresent("Usuario Registrado con Exito"); //significa que el test ha pasado con exito
+    }
+
+}
