659f81ec3016a1518e353cb5982ac525ac3ad7dc
joliva1983 <joliva1983@c3f7b990-ec2a-0410-86c9-3fd7826a91b9>
2007-03-12 13:15:48 +0000

    Test para probar el registro

---
+++ WebDocIndex/test/seleniumtest/testSignup.java
@@ -0,0 +1,27 @@
+/*
+ * testSignup.java
+ *
+ * Created on 12 de marzo de 2007, 14:09
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package seleniumtest;
+
+import com.thoughtworks.selenium.*;
+import java.util.regex.Pattern;
+
+public class testSignup extends SeleneseTestCase {
+	public void testNew() throws Exception {
+            selenium.open("http://localhost:8080/WebDocIndex/signup.html");
+            selenium.type("email1", "jsjsjsj");
+            selenium.type("email2", "sasasa");
+            selenium.type("password1", "124434");
+            selenium.type("password2", "434534545");
+            selenium.click("submit");
+            selenium.waitForPageToLoad("30000");
+            verifyTrue(selenium.isTextPresent("Los datos introducidos no son validos. Compruebe que coinciden!!"));
+            checkForVerificationErrors();
+	}
+}
