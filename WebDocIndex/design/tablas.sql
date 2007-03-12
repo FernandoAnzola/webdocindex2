613f2adc8e0710acbf0377cf7847e1f3e44dbcd5
joliva1983 <joliva1983@c3f7b990-ec2a-0410-86c9-3fd7826a91b9>
2007-03-12 12:09:32 +0000

    Añadida nueva tabla para controlar las sesiones

--- WebDocIndex/design/tablas.sql
+++ WebDocIndex/design/tablas.sql
@@ -15,4 +15,14 @@ CREATE TABLE documentos (
   PRIMARY KEY(id_documento)
 )ENGINE = InnoDB;
 
+CREATE TABLE  sesiones (
+  id_sesion int(30) unsigned NOT NULL auto_increment,
+  id_usuario int(11) unsigned NOT NULL default '0',
+  codigo varchar(100) character set latin1 NOT NULL default '',
+  ip varchar(15) character set latin1 NOT NULL default '',
+  tiempo bigint(20) unsigned NOT NULL default '0',
+  PRIMARY KEY  (id_sesion)
+) ENGINE=InnoDB;
+
 ALTER TABLE documentos ADD CONSTRAINT FK_DOCUMENTOS_USUARIOS FOREIGN KEY FK_DOCUMENTOS_USUARIOS (id_usuario) REFERENCES usuarios (id_usuario) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE sesiones ADD CONSTRAINT FK_SESIONES_USUARIOS  FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario) ON DELETE CASCADE ON UPDATE CASCADE;
